c555d9521b6b33c5dd53b88f93f6f0290f086a7b
Vivek JADHAV <5210322+VickiiHub@users.noreply.github.com>
2023-07-28 19:48:24 +0530

    Update about template literal text.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,6 @@ const D = document
 const angular = `<a href="https://www.angular.io" style="color:lightcoral;"><b>Angular</b></a>`
 const nestJS = `<a href="https://www.nestjs.com" style="color:lightcoral;"><b>NestJS</b></a>`
 const postgreSQL = `<a href="https://www.nestjs.com" style="color:lightcoral;"><b>PostgeSQL</b></a>`
-const about = `Hello, I'm <b>Vivek</b>, I've been making based web software using out of the shelf technologies i.e., ${angular}, ${nestJS} & ${postgreSQL}. Earlier I was a <i>Freelance</i> <b>iOS Developer</b> but later switched to web developement for <b>FREEDOM</b>.`
+const about = `Hello, I'm <b>Vivek</b>, I've been making web based software using out of the shelf technologies i.e., ${angular}, ${nestJS} & ${postgreSQL}. Earlier I was a <i>Freelance</i> <b>iOS Developer</b> but later switched to web developement for <b>FREEDOM</b>.`
 
 D.getElementById("ABOUT").innerHTML = about
