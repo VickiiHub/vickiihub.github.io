7806d54cc0236573a7f2d2c58bb8c9eae3731749
Vivek JADHAV <5210322+VickiiHub@users.noreply.github.com>
2023-07-28 06:09:19 +0530

    Begin Initial Upload Of My Website

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// ---- CONFIGURATIONS ----
+const print = console.log
+const D = document
+
+// ---- DOM Manipulation ----
+const angular = `<a href="https://www.angular.io"><b>Angular</b></a>`
+const nestJS = `<a href="https://www.nestjs.com"><b>NestJS</b></a>`
+const postgreSQL = `<a href="https://www.nestjs.com"><b>PostgeSQL</b></a>`
+const about = `Hello, I'm <b>Vivek</b>, I've been making based web software using out of the shelf technologies i.e., ${angular}, ${nestJS} & ${postgreSQL}. Earlier I was a <i>Freelance</i> <b>iOS Developer</b> but later switched to web developement for <b>FREEDOM</b>.`
+
+D.getElementById("ABOUT").innerHTML = about
+aboutEL.innerHTML = about
