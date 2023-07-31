52520856842a5691e32d9b6ac76b5069c53a8f8d
Vivek JADHAV <5210322+VickiiHub@users.noreply.github.com>
2023-08-01 05:26:20 +0530

    Update bio.js

--- src/bio.js
+++ src/bio.js
@@ -6,7 +6,7 @@ const D = document
 // ---bio.html---
 const bioPageHeading = `About me, just a little bit ... `
 const bioParaOne = `Hello and नमस्ते, I'm Vivek and I make web based software. I'm from India, born and raised in Saharanpur and living in Pune.`
-const bioParaTwo = `I'm a college dropout, started doing job as in manufacturing sector as assembly line worker, later on as a SAP Operator, saved money and bought MacBook Pro 13 Inch (Mid-2012) Model. Because I was very passionate about developing apps and started taking freelance work.`
+const bioParaTwo = `I'm a college dropout, started doing job in manufacturing sector as assembly line worker, later on as a SAP Operator, saved money and bought MacBook Pro 13 Inch (Mid-2012) Model.I was very passionate about developing apps and started taking freelance work.`
 const bioParaThree = `Later in the mid of 2021, I also started learning web development and realized that web development offers way much FREEDOM than native app development.`
 const bioParaFour = `My web development tech stack majorly includes (but not limited to), <a href="https://www.angular.io" target="_blank">Angular</a> 12 for Frontend, <a href="https://www.nestjs.com" target="_blank">NestJS</a> for Backend and <a href="https://www.postgresql.org" target="_blank">PostgreSQL</a> for Database.`
 const bioParaFive = `Currently, I'm learning <a href="https://www.angular.io" target="_blank">Angular</a> 16, Data visualization in JavaScript using <a href="https://d3js.org" target="_blank">D3JS</a> & web animation using <a href="https://greensock.com/gsap/" target="_blank">GSAP</a>.`
